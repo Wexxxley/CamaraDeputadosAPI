e202c453ae0b84b4292d727ee525139e0b4b914c
Wesley Freitas <wesleyfr.sobrinho@gmail.com>
2025-07-10 14:54:55 -0300

    finalizing db schema

--- models/votacao_proposicao.py
+++ models/votacao_proposicao.py
@@ -5,7 +5,6 @@ class VotacaoProposicao(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     id_proposicao: int = Field(foreign_key="proposicao.id", index=True, description="ID da proposição associada.")
     id_votacao: int = Field(foreign_key="sessaovotacao.id", index=True, description="ID da sessão de votação associada.")
-    tipo_relacao: Optional[str] = Field(default=None, max_length=50, description="Tipo de relação entre a votação e a proposição (ex: 'PRINCIPAL', 'AFETADA').")
 
     proposicao: "Proposicao" = Relationship(back_populates="votacoes_proposicao")
     sessao_votacao: "SessaoVotacao" = Relationship(back_populates="votacoes_proposicao")
--- models/voto_individual.py
+++ models/voto_individual.py
@@ -4,17 +4,13 @@ from sqlmodel import Field, SQLModel, Relationship
 
 class VotoIndividual(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    id_dados_abertos: Optional[int] = Field(default=None, index=True, unique=True)
-
     id_votacao: int = Field(foreign_key="sessaovotacao.id", index=True)
     id_deputado: int = Field(foreign_key="deputado.id", index=True)
-    id_proposicao: int = Field(foreign_key="proposicao.id", index=True)
-    
-    tipo_voto: str = Field(max_length=20, description="Sim, Não, Abstenção, Obstrução, Ausente")
-    data_hora_registro: Optional[datetime] = Field(default=None)
+    tipo_voto: str = Field(max_length=50, description="Sim, Não, Abstenção, Obstrução, Ausente")
+    data_hora_registro: Optional[str] = Field(default=None)
     sigla_partido_deputado: Optional[str] = Field(default=None, max_length=50)
     uri_deputado: Optional[str] = Field(default=None, max_length=500)
+    uri_sessao_votacao:  Optional[str] = Field(default=None, max_length=500)
 
     votacao: "SessaoVotacao" = Relationship(back_populates="votos")
     deputado: "Deputado" = Relationship(back_populates="votos_individuais")
-    proposicao: "Proposicao" = Relationship(back_populates="votos_individuais")
